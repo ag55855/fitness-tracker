97c0d5f3d0a423d04aaf5339784ff0424379d762
ahmed <you@example.com>
2025-02-23 18:15:18 +0200

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/fitness-tracker/',  
-
-// })
-
-
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/fitness-tracker/', // استخدم اسم المستودع هنا
+  base: '/fitness-tracker/', 
 })
